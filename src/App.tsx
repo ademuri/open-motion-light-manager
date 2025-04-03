08f99213b488184df4e14c79d208c943231e18b0
Adam DeMuri <adam.demuri@gmail.com>
2025-04-03 16:28:05 -0600

    Add basic serial port chooser

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import SerialPortSelector from "./components/SerialPortSelector";
 
 function App() {
 
   return (
     <>
-      <div>Test div, please ignore</div>
+      <SerialPortSelector></SerialPortSelector>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/SerialPortSelector/index.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+function SerialPortSelector() {
+  const [selectedPort, setSelectedPort] = useState(null);
+
+  const onPortChange = () => {
+    navigator.serial.getPorts().then((ports) => {
+      if (ports.length > 0) {
+        setSelectedPort(ports[0]);
+      }
+    });
+  };
+
+  navigator.serial.addEventListener("connect", (e) => {
+    onPortChange();
+  });
+
+  navigator.serial.addEventListener("disconnect", (e) => {
+    onPortChange();
+  });
+
+  onPortChange();
+
+  const handleChoosePort = () => {
+    navigator.serial
+      .requestPort({
+        filters: [
+          {
+            usbVendorId: 0x1a86,
+            usbProductId: 0x7523,
+          },
+        ],
+      })
+      .then((port) => {
+        // TODO: connect to port
+        setSelectedPort(port);
+      })
+      .catch((e) => {
+        // TODO: error handling
+        console.log(e);
+      });
+  };
+
+  const handleChangePort = () => {
+    if (selectedPort === null) {
+      return;
+    }
+    selectedPort.forget();
+    setSelectedPort(null);
+    handleChoosePort();
+  };
+
+  return (
+    <>
+      {selectedPort === null ? (
+        <button onClick={handleChoosePort}>Choose port</button>
+      ) : (
+        <>
+          <div>
+            <span>Connected</span>
+          </div>
+          <button onClick={handleChangePort}>Change port</button>
+        </>
+      )}
+    </>
+  );
+}
+
+export default SerialPortSelector;
