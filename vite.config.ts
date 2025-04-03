d34c6617079ec59124b14812676ecb64682d0cc6
Adam DeMuri <adam.demuri@gmail.com>
2025-04-03 12:35:08 -0600

    Enable HTTPS

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    basicSsl({
+      /** name of certification */
+      name: "test",
+    }),
+  ],
+  server: {
+    https: true,
+  },
+  preview: {
+    https: true,
+  },
+});
