89a059e88da5256d53fe476b04c2d1adbe1181bf
Adam DeMuri <adam.demuri@gmail.com>
2025-04-03 22:36:14 -0600

    Parse the serial response proto

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import SerialPortSelector from "./components/SerialPortSelector";
+import { SerialResponse } from "../proto_out/serial.ts";
 
 function App() {
   const [selectedPort, setSelectedPort] = useState<SerialPort | null>(null);
@@ -59,11 +60,11 @@ function App() {
           console.log("Read " + data.length + " bytes");
           const hexString = Array.from(data)
             .map((byte) => {
-              if (typeof byte !== 'number') {
+              if (typeof byte !== "number") {
                 console.error("Unexpected non-number byte:", byte);
                 return "??";
               }
-              return byte.toString(16).padStart(2, "0")
+              return byte.toString(16).padStart(2, "0");
             })
             .join(" ");
           console.log(`Data (hex): ${hexString}`);
@@ -75,10 +76,16 @@ function App() {
 
           if (data[0] != data.length - 1) {
             console.log(
-              `Data length ${data.length - 1} does not match expected length ${data[0]}`
+              `Data length ${data.length - 1} does not match expected length ${
+                data[0]
+              }`
             );
             return;
           }
+
+          const dataWithoutLength = data.subarray(1);
+          const response = SerialResponse.fromBinary(dataWithoutLength);
+          console.log(response);
         } catch (error) {
           console.error("Error reading from port:", error);
         } finally {
