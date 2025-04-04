8f7983760ea6c2ad859ef01258340b685063bd7d
Adam DeMuri <adam.demuri@gmail.com>
2025-04-04 12:14:40 -0600

    Fix CI build: explicitly compile proto

---
+++ scripts/compile-proto.js
@@ -0,0 +1,26 @@
+import { execSync } from "child_process";
+import * as fs from "fs";
+
+export function compileProto() {
+  const protoDir = "proto/";
+  const outputDir = "proto_out";
+  const protoFiles = "proto/*.proto";
+
+  // Ensure the output directory exists
+  if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir, { recursive: true });
+    console.log(`Created output directory: ${outputDir}`);
+  }
+
+  try {
+    console.log("Compiling protobuf files...");
+    execSync(
+      `npx protoc --ts_out ${outputDir} --proto_path ${protoDir} --proto_path src/proto ${protoFiles}`,
+      { stdio: "inherit" }
+    );
+    console.log("Protobuf files compiled successfully.");
+  } catch (error) {
+    console.error("Error compiling protobuf files:", error);
+    throw error; // Re-throw to stop the build
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig, Plugin } from "vite";
 import react from "@vitejs/plugin-react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
-import { execSync } from "child_process";
 import * as path from "path";
-import * as fs from "fs";
+import { compileProto } from "./scripts/compile-proto";
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,31 +17,6 @@ export default defineConfig({
 });
 
 function protobufPlugin(): Plugin {
-  const protoDir = "proto/";
-  const outputDir = "proto_out";
-  const protoFiles = "proto/*.proto";
-
-  // Helper function to compile protobuf files
-  const compileProto = () => {
-    // Ensure the output directory exists
-    if (!fs.existsSync(outputDir)) {
-      fs.mkdirSync(outputDir, { recursive: true });
-      console.log(`Created output directory: ${outputDir}`);
-    }
-
-    try {
-      console.log("Compiling protobuf files...");
-      execSync(
-        `npx protoc --ts_out ${outputDir} --proto_path ${protoDir} --proto_path src/proto ${protoFiles}`,
-        { stdio: "inherit" }
-      );
-      console.log("Protobuf files compiled successfully.");
-    } catch (error) {
-      console.error("Error compiling protobuf files:", error);
-      throw error; // Re-throw to stop the build
-    }
-  };
-
   return {
     name: "vite-plugin-protobuf",
     enforce: "pre",
